7a1b1a27e3925aa189d261e76cd027b34c1c97cb
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-09-03 13:01:15 +0200

    Added map layer

--- AirQuality/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import Index, About
-
-urlpatterns = [
-    path('air/', Index.as_view(), name="index"),
-    path('air/about/', About.as_view(), name="about"),
-]
