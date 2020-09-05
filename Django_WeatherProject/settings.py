b6da1d81b12b8f600f9b7399a03d80cb4520462d
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-09-05 17:23:02 +0200

    added static root, requirements.txt update

--- Django_WeatherProject/settings.py
+++ Django_WeatherProject/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = sk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
